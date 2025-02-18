52259368a975291231a0d6b2e048b0e00fa1240c
jamilhasan3301 <hasan15-5222@diu.edu.bd>
2025-02-18 11:22:02 +0600

    scrollview

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,35 +11,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Flutter Project Tutorial')),
-        body: Container(
-          height: double.infinity,
-          child: Row(
-
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.home,
-                size: 30,
-                color:const Color.fromARGB(255, 254, 4, 4),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Container(
+            height: 600,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: List.generate(
+                100,
+                (index) => Icon(
+                  Iconsax.shop,
+                  size: index.toDouble(),
+                  color: Colors.purple,
+                ),
               ),
-              Icon(
-                Icons.shop,
-                size: 30,
-                color:const Color.fromARGB(255, 5, 81, 244),
-              ),
-              Icon(
-                Icons.star,
-                size: 30,
-                color:const Color.fromARGB(255, 236, 0, 248),
-              ),
-              
-                
-              
-              
-              
-              
-            ],
+            ),
           ),
         ),
       ),
