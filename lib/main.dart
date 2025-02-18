a10955138f28ad9e97da9c18b44e39721be6420a
jamilhasan3301 <hasan15-5222@diu.edu.bd>
2025-02-18 12:56:30 +0600

    Nike Air Max Poster

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,78 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const SimpleNikeApp());
 
-class MyApp extends StatelessWidget {
+class SimpleNikeApp extends StatelessWidget {
+  const SimpleNikeApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text('Flutter Project Tutorial')),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Icon(
-              Icons.home,
-              size: 30,
-              color: Colors.red,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.lightGreen],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            Icon(
-              Icons.home,
-              size: 30,
-              color: Colors.red,
-            ),
-            Row(
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(
-                  Icons.home,
-                  size: 30,
-                  color: Colors.red,
+                const Text(
+                  '50% OFF',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
                 ),
-                Icon(
-                  Icons.home,
-                  size: 30,
-                  color: Colors.red,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.home,
-                      size: 30,
-                      color: Colors.red,
+                const SizedBox(height: 20),
+                Container(
+                  height: 200,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    image: const DecorationImage(
+                      image: NetworkImage('https://i.postimg.cc/05mb5106/1217812-800-800.webp'),
+                      fit: BoxFit.cover,
                     ),
-                  ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Nike Air',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  '\$150 \$75',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.yellowAccent,
+                    decoration: TextDecoration.lineThrough,
+                  ),
+                ),
+                const Text(
+                  '\$75 Only!',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
