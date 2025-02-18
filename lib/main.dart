fc9fa477c8aca8e178a850def4e52b9be9b5a977
jamilhasan3301 <hasan15-5222@diu.edu.bd>
2025-02-18 12:21:01 +0600

    row into column

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,23 +10,43 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Flutter Project Tutorial')),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Container(
-            height: 600,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: List.generate(
-                100,
-                (index) => Icon(
-                  Iconsax.shop,
-                  size: index.toDouble(),
-                  color: Colors.purple,
-                ),
-              ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(
+              Icons.home,
+              size: 30,
+              color: Colors.red,
             ),
-          ),
+            Icon(
+              Icons.home,
+              size: 30,
+              color: Colors.red,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.home,
+                  size: 30,
+                  color: Colors.red,
+                ),
+                Icon(
+                  Icons.home,
+                  size: 30,
+                  color: Colors.red,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.home,
+                      size: 30,
+                      color: Colors.red,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
